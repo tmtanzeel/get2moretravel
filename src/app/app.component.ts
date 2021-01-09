73b6f233f3c44be6b0d424003c9f43232ba60797
tanzeel <tanzeel.mirza@philips.com>
2021-01-09 22:55:01 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'get2moretravel';
+
+  checkboxes;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  onSubmit(contactForm: NgForm) {
+    console.log("called");
+    if (contactForm.valid) {
+      console.log("valid");
+      const email = contactForm.value;
+      console.log(email.email);
+      console.log(email.experience);
+      console.log(email.hear);
+      console.log(email.hotels);
+
+      console.log(email.cars);
+      console.log(email.meals);
+      console.log(email.sightseeing);
+      console.log(email.again);
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+      this.http.post('https://formspree.io/asdlf7asdf',
+        {
+          replyto: email.email,
+          experience: email.experience,
+          heardFrom: email.hear,
+          hotelAccomodation: email.hotels
+        },
+        { 'headers': headers }).subscribe(
+          response => {
+            console.log(response);
+          }
+        );
+    }
+    else {
+      console.log("form invalid");
+      
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {InputTextModule} from 'primeng/inputtext';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FeedbackFormComponent } from './feedback-form/feedback-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedbackFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    InputTextModule,
+    BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
